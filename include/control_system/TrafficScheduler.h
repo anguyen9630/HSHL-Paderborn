66215622287551a2c2bf3c03c9b5126c0b93302e
thlong-phan <72193958+thlong-phan@users.noreply.github.com>
2022-05-30 11:00:41 +0200

    traffic controller v2

--- include/control_system/TrafficScheduler.h
+++ include/control_system/TrafficScheduler.h
@@ -7,47 +7,52 @@
 #include <vector>
 #include "Task.h"
 
+/*_______________Schedule class______________*/
 /***********Definitions************/
-#define state_Idle 0
-#define state_Active 1
-
 #define occupied true
 #define unoccupied false
+
+// Block definititions
+#define BlockSE 0							//Block South East
+#define BlockNE 1							//Block North East
+#define BlockNW 2							//Block Nort West
+#define BlockSW 3							//Block South West
 /***********Class declaration************/
 class TrafficScheduler
 {
-	public:
-		// Constructor including FSM
-		TrafficScheduler();
-		// Destructor
-		~TrafficScheduler();
-
-		int fRtSizeOfMainRdRightList();
-		int fRtSizeOfMainRdLeftList();
-		int fRtSizeOfSideRdRightList();
-		int fRtSizeOfSideRdLeftList();
-
-		void fMainRoadReleaseSchedule();
-		void fSideRoadReleaseSchedule();
-		void fAddTaskToList(std::vector<Task> p_NewTaskPool);
-		std::vector<Task> v_VehicleReleaseList;
-
-	private:
-		int v_state = state_Idle;
-		std::vector<Task> v_MainRd_R_TasksList;
-		std::vector<Task> v_MainRd_L_TasksList;
-		std::vector<Task> v_SideRd_R_TasksList;
-		std::vector<Task> v_SideRd_L_TasksList;
-
-		/*bool v_MainRd_rLane = unoccupied;
-		bool v_MainRd_lLane = unoccupied;
-		bool v_SideRd_rLane = unoccupied;
-		bool v_SideRd_lLane = unoccupied;*/
-
-		bool v_TopRight = unoccupied;
-		bool v_TopLeft = unoccupied;
-		bool v_BottomRight = unoccupied;
-		bool v_BottomLeft = unoccupied;
+  public:
+    // Constructor including FSM
+  	void TrafficScheduler();
+    // Destructor
+  	void ~TrafficScheduler();
+  
+  	// Return Block states
+  	bool fRtBlockState_SE();
+  	bool fRtBlockState_NE();
+  	bool fRtBlockState_SW();
+  	bool fRtBlockState_NW();
+  
+  	std::vector<Task> fReleaseVehicle();																			
+  	void fAddTaskToList(std::vector<Task> p_NewTaskPool);
+  	bool fCheckIfBlocksAreReservable(int p_Road, int p_Direction);
+  	void fReserveBlocks(int p_Road, int p_Direction);
+  	void fUnreserveBlock(int p_Block);
+  	
+  private:  
+  	// states of 4 Blocks 
+  	bool v_BlockSE = unoccupied;
+    bool v_BlockNE = unoccupied;
+  	bool v_BlockNW = unoccupied;
+  	bool v_BlockSW = unoccupied;
+  	
+  	//Task Lists
+  	std::vector<Task> v_Rd_East_TasksList;
+  	std::vector<Task> v_Rd_West_TasksList;
+  	std::vector<Task> v_Rd_North_TasksList;
+  	std::vector<Task> v_Rd_South_TasksList;
+  
+  	unsigned long long int v_NextTaskID = 0;
+  	
 };
 
 #endif
--- src/control_system/Task.cpp
+++ src/control_system/Task.cpp
@@ -1,41 +1,51 @@
-#include "control_system/Task.h"
 
-/*_______________Task class functions______________*/
-Task::Task(int p_TaskID, int p_VehicleID, int p_Road, int p_Destination, int p_TravelDirection)
+
+
+
+/*_______________Task class______________*/
+void Task::Task(int p_VehicleID, int p_Road, int p_Direction)
 {
-	this->v_TaskID = p_TaskID;
-	this->v_VehicleID = p_VehicleID;
-	this->v_Road = p_Road;
-	this->v_Destination = p_Destination;
-	this->v_TravelDirection = p_TravelDirection;
+  this->v_VehicleID = p_VehicleID;
+  this->v_Road = p_Road;
+  this->v_Direction = p_Direction;
 }
 
-Task::~Task()
+void Task::~Task()
 {
-	// TODO: Destructor - last to do
+  // TODO: Destructor - last to do
 }
 
 int Task::fRtTaskID()
 {
-	return this->v_TaskID;
+  return this->v_TaskID;
 }
 
 int Task::fRtVehicleID()
 {
-	return this->v_VehicleID;
+  return this->v_VehicleID;
 }
 
 int Task::fRtRoad()
 {
-	return this->v_Road;
-}
-
-int Task::fRtDestination()
-{
-	return this->v_Destination;
+  return this->v_Road;
 }
 
 int Task::fRtDirection()
 {
-	return this->v_TravelDirection;
+  return this->v_Direction;
+}
+
+std::clock_t Task::fRtArrivalTime()
+{
+  return this->v_TaskArrivalTime;
+}
+
+void fAssignTaskID(int p_TaskID)
+{
+  this->v_TaskID = p_TaskID;
+}
+
+void fAssignArrivalTime(std::clock_t p_TaskArrivalTime)
+{
+  this->v_TaskArrivalTime = p_TaskArrivalTime;
 }

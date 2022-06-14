870c062b7fc3f1c4cc7830983c95beb9b4a5584e
chenshih881001 <72192211+chenshih881001@users.noreply.github.com>
2022-06-14 21:57:49 +0200

    Update traffic_control.ino

--- arduino/traffic_control/traffic_control.ino
+++ arduino/traffic_control/traffic_control.ino
@@ -36,7 +36,7 @@ void setup() {
   Serial.println("Starting Tasks pinning to core.");
   // Startup Task
   xTaskCreate(
-    Print,      // Function that should be called
+    SchedulerTask,      // Function that should be called
     "Vehicle scheduler",    // Name of the task (for debugging)
     4000,               // Stack size (bytes)
     NULL,               // Parameter to pass
@@ -47,7 +47,7 @@ void setup() {
   delay(50); 
   
   xTaskCreate(
-    Print,           // Function that should be called
+    RoadTask,           // Function that should be called
     "LED functions",    // Name of the task (for debugging)
     4000,               // Stack size (bytes)
     NULL,               // Parameter to pass
@@ -58,165 +58,169 @@ void setup() {
 }
 
 
-void SchedulerTask(void*)
+void SchedulerTask(void* param)
 {
-  if (millis() - lastMillis > 3000)
+  while(true)
   {
-    if (!random(5)) // Random chance for a car to be generated on North Road (1/5 chance)
+    if (millis() - lastMillis > 3000)
     {
-      Task TaskN(VehicleIDCount, Rd_North, random(3));  // Randomizer to decide the travel direction
-      newTasks.push_back(TaskN);
-      Serial.print("Vechicle departs from the North, VehicleID: "); //test print
-      Serial.println(VehicleIDCount);
-      VehicleIDCount++; //Update Vechicle ID
+      if (!random(5)) // Random chance for a car to be generated on North Road (1/5 chance)
+      {
+        Task TaskN(VehicleIDCount, Rd_North, random(3));  // Randomizer to decide the travel direction
+        newTasks.push_back(TaskN);
+        Serial.print("Vechicle departs from the North, VehicleID: "); //test print
+        Serial.println(VehicleIDCount);
+        VehicleIDCount++; //Update Vechicle ID
+      }
+      if(!random(5)) // Random chance for a car to be generated on East Road (1/5 chance)
+      {
+        Task TaskN(VehicleIDCount, Rd_East, random(3));
+        newTasks.push_back(TaskN);
+        Serial.print("Vechicle departs from the North, VehicleID: ");//test print
+        Serial.println(VehicleIDCount);
+        VehicleIDCount++;//Update Vechicle ID
+      }
+      if(!random(5)) // Random chance for a car to be generated on South Road (1/5 chance)
+      {
+        Task TaskN(VehicleIDCount, Rd_South, 1);
+        newTasks.push_back(TaskN);
+        Serial.print("Vechicle departs from the South, VehicleID: ");//test print
+        Serial.println(VehicleIDCount);
+        VehicleIDCount++;//Update Vechicle ID
+      }
+      if(!random(5)) // Random chance for a car to be generated on West Road (1/5 chance)
+      {
+        Task TaskN(VehicleIDCount, Rd_West, random(3));
+        newTasks.push_back(TaskN);
+        Serial.print("Vechicle departs from the West, VehicleID: ");//test print
+        Serial.println(VehicleIDCount);
+        VehicleIDCount++;//Update Vechicle ID
+      }
+      lastMillis = millis();
     }
-    if(!random(5)) // Random chance for a car to be generated on East Road (1/5 chance)
-    {
-      Task TaskN(VehicleIDCount, Rd_East, random(3));
-      newTasks.push_back(TaskN);
-      Serial.print("Vechicle departs from the North, VehicleID: ");//test print
-      Serial.println(VehicleIDCount);
-      VehicleIDCount++;//Update Vechicle ID
-    }
-    if(!random(5)) // Random chance for a car to be generated on South Road (1/5 chance)
-    {
-      Task TaskN(VehicleIDCount, Rd_South, 1);
-      newTasks.push_back(TaskN);
-      Serial.print("Vechicle departs from the South, VehicleID: ");//test print
-      Serial.println(VehicleIDCount);
-      VehicleIDCount++;//Update Vechicle ID
-    }
-    if(!random(5)) // Random chance for a car to be generated on West Road (1/5 chance)
-    {
-      Task TaskN(VehicleIDCount, Rd_West, random(3));
-      newTasks.push_back(TaskN);
-      Serial.print("Vechicle departs from the West, VehicleID: ");//test print
-      Serial.println(VehicleIDCount);
-      VehicleIDCount++;//Update Vechicle ID
-    }
-    lastMillis = millis();
-  }
-  
-  // Check button for car and create that car as a task then add it to the new task list.
-  //check for button press
-  
-  // Add cars to task list for scheduler 
-  Scheduler.fAddTaskToList(newTasks);
-  // Release next vehicle
-  std::vector<Task> NextVehicle_V = Scheduler.fReleaseVehicle();
-  if (NextVehicle_V.size())
-  {
-    Serial.print("Released a vehicle! Vehicle ID: ");
-    Serial.print(NextVehicle_V[0].fRtVehicleID());
-    Serial.print(" Road: ");
-    Serial.print(NextVehicle_V[0].fRtRoad());
-    Serial.print(" Direction: ");
-    Serial.println(NextVehicle_V[0].fRtDirection());
-    // TODO: Turn on light for released car? Maybe maybe not
-  }
     
-  
-  
-  // Reset the next task and list for next run
-  newTasks.clear();
+    // Check button for car and create that car as a task then add it to the new task list.
+    //check for button press
+    
+    // Add cars to task list for scheduler 
+    Scheduler.fAddTaskToList(newTasks);
+    // Release next vehicle
+    std::vector<Task> NextVehicle_V = Scheduler.fReleaseVehicle();
+    if (NextVehicle_V.size())
+    {
+      Serial.print("Released a vehicle! Vehicle ID: ");
+      Serial.print(NextVehicle_V[0].fRtVehicleID());
+      Serial.print(" Road: ");
+      Serial.print(NextVehicle_V[0].fRtRoad());
+      Serial.print(" Direction: ");
+      Serial.println(NextVehicle_V[0].fRtDirection());
+      // TODO: Turn on light for released car? Maybe maybe not
+    }
+      
+    
+    
+    // Reset the next task and list for next run
+    newTasks.clear(); 
+    vTaskDelay(10); 
+  }
 }
 
-void RoadTask(void*)
+void RoadTask(void* param)
 {
-  // turn off the leds & unreserve blocks if occupied after a certain amount of time
-  // else turn on light if block is occupied
-  if (Scheduler.fRtBlockState_NE() == occupied) 
+  while(true)
   {
-    if (Scheduler.v_Block_NEDirection == Rd_South && millis()-Scheduler.v_Block_NECounter > 6000)
+    // turn off the leds & unreserve blocks if occupied after a certain amount of time
+    // else turn on light if block is occupied
+    if (Scheduler.fRtBlockState_NE() == occupied) 
     {
-      digitalWrite(BlockNE_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockNE);
+      if (Scheduler.v_Block_NEDirection == Rd_South && millis()-Scheduler.v_Block_NECounter > 6000)
+      {
+        digitalWrite(BlockNE_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockNE);
+      }
+      else if (Scheduler.v_Block_NEDirection == Rd_East && millis()-Scheduler.v_Block_NECounter > 3000)
+      {
+        digitalWrite(BlockNE_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockNE);
+      }
+      else if (Scheduler.v_Block_NEDirection == Rd_West && millis()-Scheduler.v_Block_NECounter > 9000)
+      {
+        digitalWrite(BlockNE_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockNE);
+      }
+      else
+        digitalWrite(BlockNE_Light, HIGH);
     }
-    else if (Scheduler.v_Block_NEDirection == Rd_East && millis()-Scheduler.v_Block_NECounter > 3000)
+  //=================    
+    if (Scheduler.fRtBlockState_SW() == occupied) 
     {
-      digitalWrite(BlockNE_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockNE);
+      if (Scheduler.v_Block_SWDirection == Rd_North && millis()-Scheduler.v_Block_SWCounter > 6000)
+      {
+        digitalWrite(BlockSW_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockSW);
+      }
+      else if (Scheduler.v_Block_SWDirection == Rd_West && millis()-Scheduler.v_Block_SWCounter > 3000)
+      {
+        digitalWrite(BlockSW_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockSW);
+      }
+      else if (Scheduler.v_Block_SWDirection == Rd_East && millis()-Scheduler.v_Block_SWCounter > 9000)
+      {
+        digitalWrite(BlockSW_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockSW);
+      }
+      else
+        digitalWrite(BlockSW_Light, HIGH);
     }
-    else if (Scheduler.v_Block_NEDirection == Rd_West && millis()-Scheduler.v_Block_NECounter > 9000)
+    
+  //================  
+    if (Scheduler.fRtBlockState_NW() == occupied) 
     {
-      digitalWrite(BlockNE_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockNE);
+      if (Scheduler.v_Block_NWDirection == Rd_East && millis()-Scheduler.v_Block_NWCounter > 6000)
+      {
+        digitalWrite(BlockNW_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockNW);
+      }
+      else if (Scheduler.v_Block_NWDirection == Rd_North && millis()-Scheduler.v_Block_NWCounter > 3000)
+      {
+        digitalWrite(BlockNW_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockNW);
+      }
+      else if (Scheduler.v_Block_NWDirection == Rd_South && millis()-Scheduler.v_Block_NWCounter > 9000)
+      {
+        digitalWrite(BlockNW_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockNW);
+      }
+      else
+        digitalWrite(BlockNW_Light, HIGH);
     }
-    else
-      digitalWrite(BlockNE_Light, HIGH);
-  }
-//=================    
-  if (Scheduler.fRtBlockState_SW() == occupied) 
-  {
-    if (Scheduler.v_Block_SWDirection == Rd_North && millis()-Scheduler.v_Block_SWCounter > 6000)
-    {
-      digitalWrite(BlockSW_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockSW);
-    }
-    else if (Scheduler.v_Block_SWDirection == Rd_West && millis()-Scheduler.v_Block_SWCounter > 3000)
-    {
-      digitalWrite(BlockSW_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockSW);
-    }
-    else if (Scheduler.v_Block_SWDirection == Rd_East && millis()-Scheduler.v_Block_SWCounter > 9000)
-    {
-      digitalWrite(BlockSW_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockSW);
-    }
-    else
-      digitalWrite(BlockSW_Light, HIGH);
-  }
   
-//================  
-  if (Scheduler.fRtBlockState_NW() == occupied) 
-  {
-    if (Scheduler.v_Block_NWDirection == Rd_East && millis()-Scheduler.v_Block_NWCounter > 6000)
+  //================   
+    if (Scheduler.fRtBlockState_SE() == occupied)
     {
-      digitalWrite(BlockNW_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockNW);
+      if (Scheduler.v_Block_SEDirection == Rd_West && millis()-Scheduler.v_Block_SECounter > 6000)
+      {
+        digitalWrite(BlockSE_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockSE);
+      }
+      else if (Scheduler.v_Block_SEDirection == Rd_South && millis()-Scheduler.v_Block_SECounter > 3000)
+      {
+        digitalWrite(BlockSE_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockSE);
+      }
+      else if (Scheduler.v_Block_SEDirection == Rd_North && millis()-Scheduler.v_Block_SECounter > 9000)
+      {
+        digitalWrite(BlockSE_Light, LOW);
+        Scheduler.fUnreserveBlock(BlockSE);
+      }
+      else
+        digitalWrite(BlockSE_Light, HIGH);
     }
-    else if (Scheduler.v_Block_NWDirection == Rd_North && millis()-Scheduler.v_Block_NWCounter > 3000)
-    {
-      digitalWrite(BlockNW_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockNW);
-    }
-    else if (Scheduler.v_Block_NWDirection == Rd_South && millis()-Scheduler.v_Block_NWCounter > 9000)
-    {
-      digitalWrite(BlockNW_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockNW);
-    }
-    else
-      digitalWrite(BlockNW_Light, HIGH);
-  }
-
-//================   
-  if (Scheduler.fRtBlockState_SE() == occupied)
-  {
-    if (Scheduler.v_Block_SEDirection == Rd_West && millis()-Scheduler.v_Block_SECounter > 6000)
-    {
-      digitalWrite(BlockSE_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockSE);
-    }
-    else if (Scheduler.v_Block_SEDirection == Rd_South && millis()-Scheduler.v_Block_SECounter > 3000)
-    {
-      digitalWrite(BlockSE_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockSE);
-    }
-    else if (Scheduler.v_Block_SEDirection == Rd_North && millis()-Scheduler.v_Block_SECounter > 9000)
-    {
-      digitalWrite(BlockSE_Light, LOW);
-      Scheduler.fUnreserveBlock(BlockSE);
-    }
-    else
-      digitalWrite(BlockSE_Light, HIGH);
+    vTaskDelay(10);
   }
   
 }
 
-void Print(void*)
-{
-  Serial.println("Hi");
-}
 
 void loop()
 {
